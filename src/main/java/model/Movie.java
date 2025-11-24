9e89a9a0494611dfe2fcc3744f2a37935dd01b7e
ArifneedsYourHelp <arif.hamidi@stu.crc-lennox.qc.ca>
2025-11-24 13:10:11 -0500

    I am gonna break my Laptop

---
+++ src/main/java/model/Movie.java
@@ -0,0 +1,52 @@
+package model;
+
+public class Movie {
+    private String title;
+    private String genre;
+    private String duration;
+    private String rating;
+
+    public Movie(String title, String genre, String duration, String rating) {
+        this.title = title;
+        this.genre = genre;
+        this.duration = duration;
+        this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+}
